cfda92e074a70d0f820f18068091b6a5acebd001
hibohiboo <swdice66@gmail.com>
2019-03-16 17:47:28 +0900

    テストを更新

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -20,12 +20,24 @@ unitTest =
         ]
 
 
+{-| テストケースを簡単に書くためのヘルパー関数
+-}
+testParse : String -> String -> Maybe Route -> Test
+testParse name path expectedRoute =
+    test name <|
+        \_ ->
+            Url.fromString ("http://example.com" ++ path)
+                |> Maybe.andThen Route.parse
+                |> Expect.equal expectedRoute
+
+
 suite : Test
 suite =
     describe "Route"
-        [ test "should parase URL" <|
-            \_ ->
-                Url.fromString "http://example.com/"
-                    |> Maybe.andThen Route.parse
-                    |> Expect.equal (Just Route.Top)
+        [ testParse "shold parse Top" "/" (Just Route.Top)
+        , testParse "shold parse Top with quesies" "/?dummy=value" (Just Route.Top)
+        , testParse "shold parse Top with hash" "/#dumy" (Just Route.Top)
+        , testParse "shold parse User" "/foo" (Just (Route.User "foo"))
+        , testParse "shold parse Repo" "/foo/bar" (Just (Route.Repo "foo" "bar"))
+        , testParse "shold parse Inavalid path" "/foo/bar/baz" Nothing
         ]
