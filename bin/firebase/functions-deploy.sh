e054d8cddf9cfb9c847a7a6b8dde33cdf5bb5075
hibohiboo <swdice66@gmail.com>
2019-03-23 10:36:51 +0900

    functionsを追加

---
+++ bin/firebase/functions-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# このシェルスクリプトのディレクトリの絶対パスを取得。
+bin_dir=$(cd $(dirname $0) && pwd)
+
+# docker-composeの起動。 OAuth用に9005. サンプルアプリ用に5000ポートを開放。
+cd $bin_dir/../../docker && docker-compose run -p 9005:9005 -p 5000:5000 garden_firebase firebase deploy --token "$FIREBASE_TOKEN" --project garden-2a6de --only functions
+
