5aa48f8b0cc5180f058476c5a1b5439d3b61fc13
hibohiboo <swdice66@gmail.com>
2019-04-06 03:44:48 +0900

    モジュール分割

---
+++ src/assets/js/Models/Card.elm
@@ -0,0 +1,125 @@
+port module Models.Card exposing (CardData, CardLabelData, Tag, illustedBy, skillCard, tag)
+
+import Browser.Dom as Dom
+import Browser.Navigation as Navigation
+import Dict exposing (Dict)
+import GoogleSpreadSheetApi as GSAPI
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Http
+import Session
+import Skeleton exposing (viewLink, viewMain)
+import Task exposing (..)
+import Url
+import Url.Builder
+import Utils.TextStrings as Tx
+
+
+type alias CardLabelData =
+    { skill : String
+    , timing : String
+    , cost : String
+    , range : String
+    , target : String
+    , maxLevel : String
+    , level : String
+    , effect : String
+    , description : String
+    }
+
+
+type alias CardData =
+    { cardId : String
+    , cardName : String
+    , kind : String
+    , timing : String
+    , cost : Int
+    , range : Int
+    , maxRange : Int
+    , target : String
+    , maxLevel : Int
+    , effect : String
+    , description : String
+    , imgFrame : String
+    , imgMain : String
+    , tags : List Tag
+    , illustedByName : String
+    , illustedByUrl : String
+    }
+
+
+type alias Tag =
+    { name : String
+    , level : Int
+    }
+
+
+skillCard =
+    let
+        labelData =
+            CardLabelData "能力" "タイミング" "コスト" "射程" "対象" "最大Lv" "Lv" "▼効果 :" "▼解説 :"
+
+        cardData =
+            CardData "B-001" "走る" "基本能力" "アクション" 4 0 0 "自身" 1 "移動1" "逃げてもいいし、向かってもいい。\n君たちは何処にだっていける。\n一歩ずつではあるけれど。" "/assets/images/card/frame/report.gif" "/assets/images/card/main/run.png" [ Tag "移動" 0, Tag "基本能力" 0 ] "ヒューマンピクトグラム2.0" "http://pictogram2.com/"
+
+        range =
+            if cardData.range == cardData.maxRange then
+                String.fromInt cardData.range
+
+            else
+                String.fromInt cardData.range ++ " ～ " ++ String.fromInt cardData.maxRange
+    in
+    div [ class "skill-card" ]
+        [ div [ class "wrapper" ]
+            [ div [ class "base" ]
+                [ div [ class "skillLabel" ] [ text (labelData.skill ++ "/" ++ cardData.kind) ]
+                , div [ class "image" ]
+                    [ img [ src cardData.imgMain ] []
+                    , img [ src cardData.imgFrame ] []
+                    ]
+                , div [ class "cardName" ] [ text cardData.cardName ]
+                , div [ class "attrTimingLabel attrLabel border" ] [ text labelData.timing ]
+                , div [ class "attrTimingValue border" ] [ text cardData.timing ]
+                , div [ class "attrCostLabel attrLabel border" ] [ text labelData.cost ]
+                , div [ class "attrCostValue border" ] [ text (String.fromInt cardData.cost) ]
+                , div [ class "attrRangeLabel attrLabel border" ] [ text labelData.range ]
+                , div [ class "attrRangeValue border" ] [ text range ]
+                , div [ class "attrTargetLabel attrLabel border" ] [ text labelData.target ]
+                , div [ class "attrTargetValue attrLabel border" ] [ text cardData.target ]
+                , div [ class "tags" ] (List.map (\t -> tag t) cardData.tags)
+                , div [ class "mainContent border" ]
+                    [ div [ class "maxLevelLabel border" ] [ text labelData.maxLevel ]
+                    , div [ class "maxLevel border" ] [ text (String.fromInt cardData.maxLevel) ]
+                    , div [ class "lvLavel border" ] [ text labelData.level ]
+                    , div [ class "level border" ] [ text "" ]
+                    , div [ class "effect " ] [ text (labelData.effect ++ cardData.effect) ]
+                    , div [ class "description" ] [ text (labelData.description ++ cardData.description) ]
+                    ]
+                , div [ class "bottomContent" ]
+                    [ div [ class "cardId" ] [ text cardData.cardId ]
+                    , illustedBy cardData
+                    ]
+                ]
+            ]
+        ]
+
+
+illustedBy cardData =
+    div [ class "illustedBy" ]
+        [ text "illust:"
+        , a [ href cardData.illustedByUrl ] [ text cardData.illustedByName ]
+        ]
+
+
+tag : Tag -> Html msg
+tag t =
+    let
+        tagText =
+            if t.level == 0 then
+                t.name
+
+            else
+                t.name ++ ":" ++ String.fromInt t.level
+    in
+    span [ class "tag" ] [ text tagText ]
