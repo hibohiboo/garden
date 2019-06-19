0eaac75799f8747194e750bfb35d81eee838c247
hibohiboo <swdice66@gmail.com>
2019-06-19 23:50:41 +0900

    パイプラインを使用した記述に変更

--- src/assets/js/Models/CharacterListItem.elm
+++ src/assets/js/Models/CharacterListItem.elm
@@ -2,7 +2,7 @@ module Models.CharacterListItem exposing (CharacterListItem, characterListDecode
 
 import FirestoreApi as FSApi
 import Json.Decode as D exposing (Decoder)
-import Json.Decode.Pipeline
+import Json.Decode.Pipeline exposing (required)
 
 
 type alias CharacterListItem =
@@ -19,7 +19,15 @@ characterListDecoder =
 
 characterListItemDecoder : Decoder CharacterListItem
 characterListItemDecoder =
-    FSApi.fields <| D.map3 CharacterListItem (D.at [ "characterId" ] FSApi.string) (D.at [ "name" ] FSApi.string) (D.at [ "labo" ] FSApi.string)
+    D.succeed CharacterListItem
+        |> required "characterId" FSApi.string
+        |> required "name" FSApi.string
+        |> required "labo" FSApi.string
+        |> FSApi.fields
+
+
+
+-- FSApi.fields <| D.map3 CharacterListItem (D.at [ "characterId" ] FSApi.string) (D.at [ "name" ] FSApi.string) (D.at [ "labo" ] FSApi.string)
 
 
 characterListFromJson : String -> List CharacterListItem
