5d07339bd2028a0b8abf16a7a0ba545bbf94be3c
hibohiboo <swdice66@gmail.com>
2019-07-31 11:27:30 +0900

    functions deleteを更新

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,16 +2,18 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
-import characterRouter from './api/routes/characterRouter';
+// firestoreのgetapiから直接取得するようにしたため、以下は不要
+// import characterRouter from './api/routes/characterRouter';
 import { firestore } from './api/model/firestore';
 
 const app = express();
 app.use(cors({ origin: true }));
 
 // register routes
-app.use('/api/v1', characterRouter);
 
-export const itemApi = functions.https.onRequest(app);
+// app.use('/api/v1', characterRouter);
+
+// export const itemApi = functions.https.onRequest(app);
 
 
 // メモ： functions/package.jsonを書き換えるときは、docker/firebase/functions/package.jsonも同様に書き換えること。
@@ -28,10 +30,18 @@ interface RootCharacter extends Character {
 }
 
 export const onUsersCharacterCreate = functions.firestore.document('/users/{userId}/characters/{characterId}').onCreate(async (snapshot, context) => {
+  if (!context) { return; }
   await copyToRootWithUsersCharacterSnapshot(snapshot, context);
 });
 export const onUsersCharacterUpdate = functions.firestore.document('/users/{userId}/characters/{characterId}').onUpdate(async (change, context) => {
+  if (!context) { return; }
   await copyToRootWithUsersCharacterSnapshot(change.after, context);
+});
+export const onUsersCharacterDelete = functions.firestore.document('/users/{userId}/characters/{characterId}').onDelete(async (snapshot, context) => {
+  if (!context) { return; }
+  const characterId = snapshot.id;
+  await firestore.collection('publish').doc('all').collection('characters').doc(characterId).delete();
+  await firestore.collection('characters').doc(characterId).delete();
 });
 
 async function copyToRootWithUsersCharacterSnapshot(snapshot: FirebaseFirestore.DocumentSnapshot, context: functions.EventContext) {
