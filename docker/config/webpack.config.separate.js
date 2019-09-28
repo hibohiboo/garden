489384f6331c474b3337b69ef49736f642711222
hibohiboo <swdice66@gmail.com>
2019-09-28 09:08:10 +0900

    コンパイルできるように修正

--- docker/config/webpack.config.separate.js
+++ docker/config/webpack.config.separate.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const MODE = 'production';
